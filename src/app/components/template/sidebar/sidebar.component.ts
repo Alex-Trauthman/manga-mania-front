ff480c0d8d3f6e9b07497cdda77df6f4a7986a10
SlonyRufus <33912376+slonyrufus@users.noreply.github.com>
2024-11-15 19:32:56 -0300

    search & sidebar

--- src/app/components/template/sidebar/sidebar.component.ts
+++ src/app/components/template/sidebar/sidebar.component.ts
@@ -4,11 +4,12 @@ import { MatToolbar } from '@angular/material/toolbar';
 import { MatList,MatListItem,MatNavList } from '@angular/material/list';
 import { RouterModule,RouterOutlet } from '@angular/router';
 import { SidebarService } from '../../../services/sidebar.service';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
     selector: 'app-sidebar',
     standalone: true,
-    imports: [MatSidenav,MatDrawer,MatDrawerContainer,RouterModule,MatDrawerContent,MatToolbar,MatList,MatNavList,MatListItem,RouterOutlet],
+    imports: [MatIconModule,MatSidenav,MatDrawer,MatDrawerContainer,RouterModule,MatDrawerContent,MatToolbar,MatList,MatNavList,MatListItem,RouterOutlet],
     templateUrl: './sidebar.component.html',
     styleUrl: './sidebar.component.css'
 })
@@ -16,6 +17,7 @@ export class SidebarComponent implements OnInit {
     @ViewChild('drawer') public drawer!: MatDrawer;
 
     constructor(private sideBarService: SidebarService) { }
+    showFiller = false;
 
     ngOnInit(): void {
         this.sideBarService.sideNavToggleSubject.subscribe(() => {
